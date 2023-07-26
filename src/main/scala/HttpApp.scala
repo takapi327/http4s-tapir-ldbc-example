1a50060027da48db9a295f15f5b9f720a0ae7a1d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-07-26 18:45:55 +0900

    Fix HttpApp

--- src/main/scala/HttpApp.scala
+++ src/main/scala/HttpApp.scala
@@ -1,19 +1,19 @@
 
+import com.google.inject.*
+
 import cats.effect.*
 
-import org.http4s.*
-import org.http4s.dsl.io.*
 import org.http4s.ember.server.EmberServerBuilder
+
+import infrastructure.mysql.DatabaseModule
 
 object HttpApp extends ResourceApp.Forever:
 
-  private val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case GET -> Root / "healthcheck" => Ok("Healthcheck Ok")
-  }
-
   override def run(args: List[String]): Resource[IO, Unit] =
+    val injector = Guice.createInjector(new DatabaseModule)
+    val app = injector.getInstance(classOf[Routes]).router.orNotFound
     EmberServerBuilder
       .default[IO]
-      .withHttpApp(routes.orNotFound)
+      .withHttpApp(app)
       .build
       .flatMap(_ => Resource.unit)
