28d6ba825830ff34b562574f324fbc27b7ce7e93
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-09-03 23:54:26 +0900

    Fix Service

--- src/main/scala/application/service/TaskCategoryLinksService.scala
+++ src/main/scala/application/service/TaskCategoryLinksService.scala
@@ -4,10 +4,10 @@ package application.service
 import javax.sql.DataSource
 import javax.inject.Inject
 
-import cats.data.Kleisli
 import cats.effect.IO
 
 import ldbc.sql.*
+import ldbc.dsl.ConnectionIO
 import ldbc.dsl.io.*
 import ldbc.dsl.logging.{ LogEvent, LogHandler }
 
@@ -15,18 +15,31 @@ class TaskCategoryLinksService @Inject() (
   dataSource: DataSource,
 ):
 
+  // SQL文中の`?`を順にリストの値で置き換える関数
+  def replacePlaceholders(sql: String, values: List[Any]): String = {
+    val placeholders = List.fill(values.length)("?")
+    placeholders.zip(values.map(quoteValue)).foldLeft(sql)((result, pair) => result.replace(pair._1, pair._2))
+  }
+
+  // 値を適切な形式でクォートする関数
+  def quoteValue(value: Any): String = value match {
+    case stringValue: String => s"'$stringValue'"
+    case _ => value.toString
+  }
+
   given LogHandler[IO] with
     override def run(logEvent: LogEvent): IO[Unit] =
       IO.println(
         logEvent match
-          case LogEvent.Success(sql, args) => s"Successful Statement Execution: ${sql} ${args.mkString(",")}"
-          case LogEvent.ExecFailure(sql, args, failure) => s"Failed Statement Execution: ${sql} ${args.mkString(",")}" ++ failure.getMessage
+          case LogEvent.Success(sql, args) => s"Successful Statement Execution: ${replacePlaceholders(sql, args)}"
+          case LogEvent.ProcessingFailure(sql, args, failure) => s"Failed ResultSet Processing: ${replacePlaceholders(sql, args)}" ++ failure.getMessage
+          case LogEvent.ExecFailure(sql, args, failure) => s"Failed Statement Execution: ${replacePlaceholders(sql, args)} \n ${failure.getMessage}"
       )
 
   def delete(id: Long): IO[Int] =
     (for
-      categoryDeleted <- sql"DELETE FROM category WHERE id = $id".update()
+      categoryDeleted <- sql"DELETE FROM category WHERE id = $id".update
       taskDeleted <- categoryDeleted match
-        case 0 => Kleisli.pure[IO, Connection[IO], Int](0)
-        case _ => sql"DELETE FROM task WHERE category_id = $id".update()
+        case 0 => ConnectionIO.pure[IO, Int](0)
+        case _ => sql"DELETE FROM task WHERE category_id = $id".update
     yield categoryDeleted + taskDeleted).transaction.run(dataSource)
